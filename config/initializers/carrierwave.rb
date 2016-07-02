cb671270ba5fe845cae7d3f284388015aa3c682e
Nick Barone <nick.barone@gmail.com>
2016-07-02 16:14:18 -0700

    Restore user pics (using narfanator@gmail.com aws account)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['aws_secret_access_key'],
     region:                'us-west-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'swingcity'                          # required
+  config.fog_directory  = ENV['aws_bucket']                        # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
