86395b51a4ec18038745f0b01bb44b041d2b6dbb
Nick Barone <nick.barone@gmail.com>
2016-07-15 14:50:47 -0700

    Bugfix on trusted params, sort homepage by name

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
   skip_authorization_check
   def index
-    @users = User.where(status: :camp).all
+    @users = User.where(status: :camp).order(:name).all
     @starting_user = (params[:user].to_i || 1)
   end
 end
